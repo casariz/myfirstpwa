08dfe813f2554b586de6c3ee1e7639b08ecc00c0
Juan Sebastian Grajales Samudio <96671221+JuanSebastianGrajales@users.noreply.github.com>
2024-03-12 22:31:06 -0500

    Mi commit a mi línea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GGyWnycXEAE2L7f from './image/GGyWnycXEAE2L7f.jpg'; // Ajusta la extensión si no es .jpg
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>NieR Automata</h1>
+        <img src={GGyWnycXEAE2L7f} className="App-logo" alt="Nier Automata" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        NieR:Automata (ニーア オートマタ Nīa Ōtomata?) es un videojuego de rol de acción desarrollado por PlatinumGames y publicado por Square Enix para PlayStation 4, Microsoft Windows y Xbox One, así como para Nintendo Switch. Aunque en un principio la distribuidora planeó su lanzamiento para noviembre de 2016, finalmente lo publicó el 23 de febrero de 2017 en Japón.
         </p>
         <a
           className="App-link"
